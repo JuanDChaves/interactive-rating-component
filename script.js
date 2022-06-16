5fba90aa02aa831ab7af1d12debe00214e5e16d4
Juan D Chaves <juandavidchaves@protonmail.com>
2022-06-16 17:20:39 -0300

    finished 01

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 const submitBtn = document.getElementById("submit-btn");
+const numbers = document.querySelectorAll("#number");
+const result = document.getElementById("result");
+const ratingCont = document.getElementById("rating-container");
+const thankyouCont = document.getElementById("thankyou-container");
 
+let selectedNum = 0;
 
-function submitRating() {
-    console.log("holainas");
+function getSelectedNum(e) {
+
+    numbers.forEach((number) => {
+        number.classList.remove("selected-num");
+    });
+    e.target.classList.add("selected-num");
+    selectedNum = e.target.innerText;
+    result.innerText = selectedNum;
 }
 
+function submitRating() {
+    ratingCont.style.display = "none";
+    thankyouCont.style.display = "flex";
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", getSelectedNum);
+});
 submitBtn.addEventListener("click", submitRating);
